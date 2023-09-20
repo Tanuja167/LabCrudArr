354364358e84cab9d2d90d2aaa74238a5ed817c7
user <user@DESKTOP-SVU5GE4>
2023-09-20 09:53:35 +0530

    Add project files.

---
+++ LabCrudArr/ProductImplementation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCrudArr
+{
+    public class Product
+    {
+        int id;
+        String name;
+        int price;
+        public Product(int id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            return id + " :" + name + " " + price;
+        }
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+    }
+
+    public class ProductImplementation
+    {
+        Product[] product;
+        int count;
+        public ProductImplementation()
+        {
+            product = new Product[5];
+            count = 0;
+        }
+        public void AddProduct(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter the id");
+                int pid = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Product name");
+                String pname = Console.ReadLine();
+                Console.WriteLine("Entre the price");
+                int price = int.Parse(Console.ReadLine());
+                product[count] = new Product(pid, pname, price);
+                count++;
+            }
+        }
+        public void ShowAllProduct()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (product[i] != null)
+                {
+                    Console.WriteLine(product[i]);
+                }
+            }
+        }
+        public void SearchProductById(int id)
+        {
+            for (int i = 0; i < product.Length; i++)
+            {
+                if (product[i] != null)
+                {
+                    if (product[i].Id == id)
+                    {
+                        Console.WriteLine(product[i]);
+                    }
+                    if (product[i].Id != id)
+                    {
+                        Console.WriteLine("Product is not prsent: ");
+                    }
+                }
+            }
+        }
+        public void UpadteProduct(int id, int newprice)
+        {
+            for (int i = 0; i < product.Length; i++)
+            {
+                if (product[i] != null)
+                {
+                    if (product[i].Id == id)
+                    {
+                        product[i].Price = newprice;
+                    }
+
+                    if (product[i].Id != id)
+                    {
+                        Console.WriteLine("Id not match: ");
+                    }
+                }
+            }
+        }
+        public void DeleteProduct(int id)
+        {
+            for (int i = 0; i < product.Length; i++)
+            {
+                if (product[i] != null)
+                {
+                    if (product[i].Id == id)
+                    {
+                        product[i] = null;
+                    }
+                    if (product[i].Id != id)
+                    {
+                        Console.WriteLine("This product is not Available:"); ;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
---
+++ LabCrudArr/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCrudArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductImplementation product = new ProductImplementation();
+            char ch = ' ';
+            do
+            {
+                Console.WriteLine("1.Add\n2.Show\n3.Update\n4.Delete\n 5.Serach");
+                Console.WriteLine("enter the choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("How many products you want to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        product.AddProduct(n);
+                        break;
+                    case 2:
+                        Console.WriteLine("////////////////////");
+                        product.ShowAllProduct();
+                        break;
+                    case 3:
+                        Console.WriteLine("enter the id for update");
+                        int pid = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the new price");
+                        int price = int.Parse(Console.ReadLine());
+                        product.UpadteProduct(pid, price);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the id for delete");
+                        int dbid = Convert.ToInt32(Console.ReadLine());
+                        product.DeleteProduct(dbid);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter id for search");
+                        int id = int.Parse(Console.ReadLine());
+                        product.SearchProductById(id);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Operation");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue....");
+                ch = Convert.ToChar(Console.ReadLine());
+
+            } while (ch == 'y' || ch == 'Y');
+
+
+
+
+
+
+            
+
+        }
+    }
+}
+
